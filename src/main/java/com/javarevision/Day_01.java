d91ac2c30c3b2f3e9359f615a28d5b564f73a2ee
the_codex_ <138112742+codingguru2221@users.noreply.github.com>
2026-02-03 22:28:06 +0530

    Day-1

---
+++ src/main/java/com/javarevision/Day_01.java
@@ -0,0 +1,34 @@
+package com.javarevision;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class Day_01 {
+
+    private byte b;
+
+    void DataType() {
+        //        DataTypes advance
+//        primitive data type
+//        integral
+        int a;
+        byte b;
+        short c;
+        long l;
+//        floating point
+        float fl;
+        double db;
+//        Non-numeric
+        boolean bl;
+        char ch;
+//        non-primitive data type
+        String sc;
+        Array arr;
+        List list;
+    }
+
+    public static void main(String[] args) {
+        
+
+    }
+}
